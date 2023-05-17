0308ba3351bb8c9218bf0ef77be2965eff6bd82b
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-16 21:46:09 -0400

    print band name function + adj

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 print("Looking for a new band name? I've got u covered! \n\n")
 
+adj = input("Please give me an adjective to describe your band's aesthetic:\n")
+
 city = input("What city did you grow up in?\n")
 
 pet = input("What is the name of either your current pet or a pet you have previosly owned?\n")
 
-print("Your band name is: " + city + " " + pet)
+print("Your band name is: " + adj + " " + city + " " + pet + "s")
