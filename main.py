2daaf1c9cab1984abca0da438690849e96360fa4
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-16 21:33:24 -0400

    question 2

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 print("Looking for a new band name? I've got u covered! \n\n")
 
 city = input("What city did you grow up in?\n")
+
+pet = input("What is the name of either your current pet or a pet you have previosly owned?\n")
