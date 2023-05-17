e60ebab39ae613ce6a77f89ce7bc36812dfa8ff2
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-05-16 21:49:30 -0400

    edit q1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Looking for a new band name? I've got u covered! \n\n")
 
-adj = input("Please give me an adjective to describe your band's aesthetic:\n")
+adj = input("Please give me an adjective to describe the members of your bad or your band's aesthetic:\n")
 
 city = input("What city did you grow up in?\n")
 
